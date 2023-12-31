bcc0f4bd51a5600c0d9940d65fd99cf2e8cb7ad1
AndreMonteiro <andrefmonteiro@outlook.com>
2023-12-31 23:19:50 +0000

    added resolution for array123 problem

---
+++ src/warmup2/Array123.java
@@ -0,0 +1,24 @@
+package warmup2;
+
+public class Array123 {
+    /*
+    Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+
+        array123([1, 1, 2, 3, 1]) → true
+        array123([1, 1, 2, 4, 1]) → false
+    array123([1, 1, 2, 1, 2, 3]) → true
+     */
+
+    public boolean array123(int[] nums) {
+        // iterate through the array and check index values
+        // check 3 consecutive indices for values 1,2,3
+
+        for (int i = 0; i < nums.length - 2; i++){
+
+            if (nums[i] == 1 && nums[i + 1] == 2 && nums[i + 2] == 3){
+                return true;
+            }
+        }
+        return false;
+    }
+}
